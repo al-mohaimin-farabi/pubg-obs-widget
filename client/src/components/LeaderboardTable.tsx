3c71e192437c07f03f571e02f98472ce56ef12cb
Al Mohaimin Farabi <73338180+al-mohaimin-farabi@users.noreply.github.com>
2025-12-19 15:58:16 +0600

    skin added

--- client/src/components/LeaderboardTable.tsx
+++ client/src/components/LeaderboardTable.tsx
@@ -8,29 +8,30 @@ const LeaderBoardTable = ({ className }: { className?: string }) => {
   return (
     <div
       className={cn(
-        "font-oswald max-w-196.25 min-w-196.25 overflow-hidden",
+        "font-oswald relative max-w-196.25 min-w-196.25 overflow-hidden",
         className
       )}
     >
-      <div className="leaderboard-grid relative grid w-full gap-1 font-semibold">
+      <div className="leaderboard-grid relative z-10 grid w-full gap-1 font-semibold">
         {/* Header Row */}
-        <div className="grid grid-cols-[60px_1fr_80px_60px_80px_60px] gap-1 bg-linear-to-r from-widget-muted from-30% to-widget-accent py-1 text-nowrap text-widget-secondary">
-          <div className="grid max-h-6 place-content-center overflow-hidden px-2 text-center text-sm">
+
+        <div className="relative z-10 grid grid-cols-[60px_1fr_80px_60px_80px_60px] gap-1 text-nowrap text-widget-secondary">
+          <div className="grid h-7 place-content-center overflow-hidden px-2 text-center text-sm">
             RANK
           </div>
-          <div className="grid max-h-6 items-center overflow-hidden px-2 text-left text-sm">
+          <div className="grid h-7 items-center overflow-hidden px-2 text-left text-sm">
             TEAM
           </div>
-          <div className="grid max-h-6 place-content-center overflow-hidden px-2 text-center text-sm">
+          <div className="grid h-7 place-content-center overflow-hidden px-2 text-center text-sm">
             PLACE PTS
           </div>
-          <div className="grid max-h-6 place-content-center overflow-hidden px-2 text-center text-sm">
+          <div className="grid h-7 place-content-center overflow-hidden px-2 text-center text-sm">
             ELIMS
           </div>
-          <div className="grid max-h-6 place-content-center overflow-hidden px-2 text-center text-sm">
+          <div className="grid h-7 place-content-center overflow-hidden px-2 text-center text-sm">
             TOTAL PTS
           </div>
-          <div className="grid max-h-6 place-content-center overflow-hidden px-2 text-center text-sm">
+          <div className="grid h-7 place-content-center overflow-hidden px-2 text-center text-sm">
             <img
               src="/chicken.png"
               className="mx-auto w-full overflow-hidden"
@@ -39,52 +40,50 @@ const LeaderBoardTable = ({ className }: { className?: string }) => {
           </div>
         </div>
 
-        {/* Data Rows */}
         {leaderboard.map((team, index) => (
           <div
             key={team.team_id}
             className={cn(
-              "grid max-h-11.5 grid-cols-[60px_56px_20px_1fr_80px_60px_80px_60px] gap-1 bg-linear-to-r from-widget-accent from-60% to-widget-muted/80 text-white",
+              "relative z-10 grid max-h-11.5 grid-cols-[60px_56px_20px_1fr_80px_60px_80px_60px] gap-1 text-white",
               index < leaderboard.length - 1 ? "" : ""
             )}
           >
-            <div className="flex h-11.5 max-h-11.5 items-center justify-center overflow-hidden bg-widget-muted py-1 text-center text-nowrap">
+            <div className="flex h-11.5 max-h-11.5 items-center justify-center overflow-hidden">
               <span className="text-lg font-bold">#{team.position}</span>
             </div>
-            <div className="grid h-11.5 max-h-11.5 place-content-center overflow-hidden px-2 py-1">
+            <div className="grid h-11.5 max-h-11.5 place-content-center overflow-hidden px-2">
               <img src="/bd-flag.webp" className="w-14" alt="" />
             </div>
-            <div className="flex h-11.5 max-h-11.5 items-center justify-center overflow-hidden py-2">
+            <div className="flex h-11.5 max-h-11.5 items-center justify-center overflow-hidden">
               <Separator
-                className="rounded bg-white data-[orientation=vertical]:w-0.5"
+                className="rounded bg-white data-[orientation=vertical]:h-[80%] data-[orientation=vertical]:w-0.5"
                 orientation="vertical"
               />
             </div>
-            <div className="flex h-11.5 max-h-11.5 items-center overflow-hidden px-2 py-1 text-sm">
+            <div className="flex h-11.5 max-h-11.5 items-center overflow-hidden px-2 text-center text-lg">
               <div className="flex items-center gap-2">
                 <img
                   className="h-12 w-12 object-cover"
                   src={`${team?.team_logo}`}
                 />
-                <span className="shrink-0 text-lg uppercase">
-                  {team.team_name}
-                </span>
+                <span className="shrink-0 uppercase">{team.team_name}</span>
               </div>
             </div>
-            <div className="grid h-11.5 max-h-11.5 place-content-center overflow-hidden px-2 py-1 text-center text-base">
+            <div className="grid h-11.5 max-h-11.5 place-content-center overflow-hidden px-2 text-center text-base">
               {team.placement_point || 0}
             </div>
-            <div className="grid h-11.5 max-h-11.5 place-content-center overflow-hidden px-2 py-1 text-center text-base">
+            <div className="grid h-11.5 max-h-11.5 place-content-center overflow-hidden px-2 text-center text-base">
               {team.kills || 0}
             </div>
-            <div className="grid h-11.5 max-h-11.5 place-content-center overflow-hidden px-2 py-1 text-center text-base">
+            <div className="grid h-11.5 max-h-11.5 place-content-center overflow-hidden px-2 text-center text-base">
               {team.total_points || 0}
             </div>
-            <div className="grid h-11.5 max-h-11.5 place-content-center overflow-hidden px-2 py-1 text-center text-base">
+            <div className="grid h-11.5 max-h-11.5 place-content-center overflow-hidden px-2 text-center text-base">
               {team.games_won || 0}
             </div>
           </div>
         ))}
+        <img src="/skin.png" className="absolute inset-0" alt="" />
       </div>
     </div>
   );
